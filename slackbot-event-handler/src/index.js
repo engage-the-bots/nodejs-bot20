f36809b4a316692426b5d0825436609663aed28d
Matthew De la Rosa <mldelaro@asu.edu>
2023-09-12 08:33:40 -0700

    Add dummy log

--- slackbot-event-handler/src/index.js
+++ slackbot-event-handler/src/index.js
@@ -1,58 +1,59 @@
-const { App, AwsLambdaReceiver } = require('@slack/bolt');
-
-// Initialize your custom receiver
-const awsLambdaReceiver = new AwsLambdaReceiver({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-});
-
-// Initializes your app with your bot token and the AWS Lambda ready receiver
-const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    receiver: awsLambdaReceiver,
-
-    // When using the AwsLambdaReceiver, processBeforeResponse can be omitted.
-    // If you use other Receivers, such as ExpressReceiver for OAuth flow support
-    // then processBeforeResponse: true is required. This option will defer sending back
-    // the acknowledgement until after your handler has run to ensure your handler
-    // isn't terminated early by responding to the HTTP request that triggered it.
-
-    // processBeforeResponse: true
-
-});
-
-// Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
-    // say() sends a message to the channel where the event was triggered
-    await say({
-        blocks: [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": `Hey there <@${message.user}>!`
-                },
-                "accessory": {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Click Me"
-                    },
-                    "action_id": "button_click"
-                }
-            }
-        ],
-        text: `Hey there <@${message.user}>!`
-    });
-});
-
-app.action('button_click', async ({ body, ack, say }) => {
-    // Acknowledge the action
-    await ack();
-    await say(`<@${body.user.id}> clicked the button`);
-});
+// const { App, AwsLambdaReceiver } = require('@slack/bolt');
+//
+// // Initialize your custom receiver
+// const awsLambdaReceiver = new AwsLambdaReceiver({
+//     signingSecret: process.env.SLACK_SIGNING_SECRET,
+// });
+//
+// // Initializes your app with your bot token and the AWS Lambda ready receiver
+// const app = new App({
+//     token: process.env.SLACK_BOT_TOKEN,
+//     receiver: awsLambdaReceiver,
+//
+//     // When using the AwsLambdaReceiver, processBeforeResponse can be omitted.
+//     // If you use other Receivers, such as ExpressReceiver for OAuth flow support
+//     // then processBeforeResponse: true is required. This option will defer sending back
+//     // the acknowledgement until after your handler has run to ensure your handler
+//     // isn't terminated early by responding to the HTTP request that triggered it.
+//
+//     // processBeforeResponse: true
+//
+// });
+//
+// // Listens to incoming messages that contain "hello"
+// app.message('hello', async ({ message, say }) => {
+//     // say() sends a message to the channel where the event was triggered
+//     await say({
+//         blocks: [
+//             {
+//                 "type": "section",
+//                 "text": {
+//                     "type": "mrkdwn",
+//                     "text": `Hey there <@${message.user}>!`
+//                 },
+//                 "accessory": {
+//                     "type": "button",
+//                     "text": {
+//                         "type": "plain_text",
+//                         "text": "Click Me"
+//                     },
+//                     "action_id": "button_click"
+//                 }
+//             }
+//         ],
+//         text: `Hey there <@${message.user}>!`
+//     });
+// });
+//
+// app.action('button_click', async ({ body, ack, say }) => {
+//     // Acknowledge the action
+//     await ack();
+//     await say(`<@${body.user.id}> clicked the button`);
+// });
 
 // Handle the Lambda function event
 module.exports.lambda_handler = async (event, context, callback) => {
-    const handler = await awsLambdaReceiver.start();
-    return handler(event, context, callback);
+    console.log('on handler')
+    // const handler = await awsLambdaReceiver.start();
+    // return handler(event, context, callback);
 }
