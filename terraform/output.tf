9c43caeb9cf3b401e61fe4a2d64669754f518bc7
Matthew De la Rosa <mldelaro@asu.edu>
2023-09-12 00:30:58 -0700

    Fixes tf-output

--- terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "slackbot_handler_invocation_url" {
-  description = "The endpoint of the slackbot-handler lambda function"
-  value       = aws_lambda_function_url.handler_endpoint.function_url
-}
