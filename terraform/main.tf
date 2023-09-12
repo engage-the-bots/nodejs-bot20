c523605998c9cfe263a3866524ce97f8b6725c5c
Matthew De la Rosa <mldelaro@asu.edu>
2023-09-12 15:24:05 -0700

    Fixes refs to lambda handler

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ module "bot_lambda" {
   source = "terraform-aws-modules/lambda/aws"
   function_name = var.slackbot_resource_name
   description   = "Engage 2023 Build your own NodeJS Slack Bot"
-  handler       = "src/app.lambda_handler"
+  handler       = "src/index.handler"
   runtime       = "nodejs18.x"
   source_path = "../slackbot-event-handler"
   create_lambda_function_url = true
