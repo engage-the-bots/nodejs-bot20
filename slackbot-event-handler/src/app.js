2b1a1a03e76606f21af409c0e63264741d2650ed
Matthew De la Rosa <mldelaro@asu.edu>
2023-09-12 21:19:11 -0700

    Add more logging

--- slackbot-event-handler/src/app.js
+++ slackbot-event-handler/src/app.js
@@ -9,40 +9,33 @@ const awsLambdaReceiver = new AwsLambdaReceiver({
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     receiver: awsLambdaReceiver,
-
-    // When using the AwsLambdaReceiver, processBeforeResponse can be omitted.
-    // If you use other Receivers, such as ExpressReceiver for OAuth flow support
-    // then processBeforeResponse: true is required. This option will defer sending back
-    // the acknowledgement until after your handler has run to ensure your function
-    // isn't terminated early by responding to the HTTP request that triggered it.
-
-    // processBeforeResponse: true
 });
 
-// Listens to incoming messages that contain "hello"
-// app.message('hello', async ({ message, say }) => {
-//     // say() sends a message to the channel where the event was triggered
-//     await say({
-//         blocks: [
-//             {
-//                 "type": "section",
-//                 "text": {
-//                     "type": "mrkdwn",
-//                     "text": `Hey there <@${message.user}>!`
-//                 },
-//                 "accessory": {
-//                     "type": "button",
-//                     "text": {
-//                         "type": "plain_text",
-//                         "text": "Click Me"
-//                     },
-//                     "action_id": "button_click"
-//                 }
-//             }
-//         ],
-//         text: `Hey there <@${message.user}>!`
-//     });
-// });
+Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    console.log('on hello');
+    await say({
+        blocks: [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": `Hey there <@${message.user}>!`
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Click Me"
+                    },
+                    "action_id": "button_click"
+                }
+            }
+        ],
+        text: `Hey there <@${message.user}>!`
+    });
+});
 
 app.event('app_home_opened', ({ event, say }) => {
     say(`Hello world, <@${event.user}>!`);
@@ -58,6 +51,7 @@ app.action('button_click', async ({ body, ack, say }) => {
 // Listens to incoming messages that contain "goodbye"
 app.message('goodbye', async ({ message, say }) => {
     // say() sends a message to the channel where the event was triggered
+    console.log('on goodbye');
     await say(`See ya later, <@${message.user}> :wave:`);
 });
 
