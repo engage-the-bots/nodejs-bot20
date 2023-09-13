4f0b47ab1843b4bbf18b14daf68bb2b05c207a47
Matthew De la Rosa <mldelaro@asu.edu>
2023-09-13 09:16:46 -0700

    Winter is coming

--- slackbot-event-handler/src/app.js
+++ slackbot-event-handler/src/app.js
@@ -14,8 +14,10 @@ const app = new App({
 
 let keywordMemeMap = {
     'this-is-fine': { template: '/fine', resourcePathParms: 2, type: '.png', title: 'This is fine', memeString: '_this-is-fine&top&bottom_', exampleImageUrl: 'https://api.memegen.link/images/fine/top/bottom.png'}, //https://api.memegen.link/images/fine/_/this_is_fine.png
-    'panik-kalm-panik': { template: '/panik-kalm-panik', resourcePathParms: 3, type: '.png', title: 'Panik Kalm PANIK', memeString: '_kalm-panik-kalm&You_hear_a_sound_downstairs/It\'s_just_a_cat/You_don\'t_have_a_cat_', exampleImageUrl: 'https://api.memegen.link/images/panik-kalm-panik/You_hear_a_sound_downstairs/It\'s_just_a_cat/You_don\'t_have_a_cat.png'}, //https://api.memegen.link/images/panik-kalm-panik/You_hear_a_sound_downstairs/It's_just_a_cat/You_don't_have_a_cat.png
-    // 'patrick', // https://api.memegen.link/images/patrick/why_don't_we_take_all_the_memes/and_put_them_on_memegen.png
+    'panik-kalm-panik': { template: '/panik-kalm-panik', resourcePathParms: 3, type: '.png', title: 'Panik Kalm PANIK', memeString: '_kalm-panik-kalm&you hear a sound downstairs&It\'s just a cat&you don\'t have a cat_', exampleImageUrl: 'https://api.memegen.link/images/panik-kalm-panik/You_hear_a_sound_downstairs/It\'s_just_a_cat/You_don\'t_have_a_cat.png'}, //https://api.memegen.link/images/panik-kalm-panik/You_hear_a_sound_downstairs/It's_just_a_cat/You_don't_have_a_cat.png
+    'winter-is-coming': { template: '/winter', resourcePathParms: 2, type: '.png', title: 'Winter is coming', memeString: '_winter-is-coming&prepare yourself&winter is coming', exampleImageUrl: 'https://api.memegen.link/images/panik-kalm-panik/You_hear_a_sound_downstairs/It\'s_just_a_cat/You_don\'t_have_a_cat.png'}, //https://api.memegen.link/images/panik-kalm-panik/You_hear_a_sound_downstairs/It's_just_a_cat/You_don't_have_a_cat.png
+    //https://api.memegen.link/images/winter/prepare_yourself/winter_is_coming.png
+    // https://api.memegen.link/images/patrick/why_don't_we_take_all_the_memes/and_put_them_on_memegen.png
     // https://api.memegen.link/images/right/Senior_Developer/Junior_Developer/Put_it_in_the_backlog./So_we_can_fix_it_later,_right~q/So_we_can_fix_it_later,_right~q.png
     // https://api.memegen.link/images/yodawg/yo_dawg/i_heard_you_like_memes.png
     // https://api.memegen.link/images/dbg/Clicking_the_'X'_on_a_mobile_ad/The_'X'_is_part_of_the_ad.png
